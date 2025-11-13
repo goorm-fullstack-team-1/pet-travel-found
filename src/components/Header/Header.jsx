2a753b89dc5e04545ef681fe789665cf9d80bea8
yuhyeon99 <jujini31@gmail.com>
2025-11-13 13:45:33 +0900

    feat: 로그인 버튼 추가

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import "./Header.css";
 import logo from "../../assets/images/logo.png";
+import AuthIcon from "../../assets/images/icons/auth_icon.svg";
+import Button from "../Button/Button";
 
 const Header = () => {
   return (
@@ -19,6 +21,13 @@ const Header = () => {
             </li>
             <li>
               <Link to="/my-page">마이페이지</Link>
+            </li>
+            <li>
+              <Button>
+                <img src={AuthIcon} alt="auth-icon" title="auth-icon" />
+                로그인
+                {/* TODO: 추후에는 로그인 상태에 따라 동적으로 '로그인' 또는 '로그아웃' 텍스트가 보이도록 수정해야함. */}
+              </Button>
             </li>
           </ul>
         </div>
