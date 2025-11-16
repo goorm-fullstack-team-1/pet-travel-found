aac9d728ce7efcfd73d932d84b0e2fd85e0af8c0
yuhyeon99 <jujini31@gmail.com>
2025-11-17 00:01:32 +0900

    refactor(header): css 를 module.css로 변경 및 적용

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import "./Header.css";
+import styles from "./Header.module.css";
 import logo from "../../assets/images/logo.png";
 import AuthIcon from "../../assets/images/icons/auth_icon.svg";
 import Button from "../Button/Button";
@@ -7,11 +7,11 @@ import Button from "../Button/Button";
 const Header = () => {
   return (
     <header>
-      <div className="header_inner">
-        <div className="logo_area">
+      <div className={styles.header_inner}>
+        <div className={styles.logo_area}>
           <img src={logo} alt="Logo" />
         </div>
-        <div className="nav">
+        <div className={styles.nav}>
           <ul>
             <li>
               <Link to="/">메인페이지</Link>
@@ -23,7 +23,7 @@ const Header = () => {
               <Link to="/my-page">마이페이지</Link>
             </li>
             <li>
-              <Button className="auth_button">
+              <Button className={styles.auth_button}>
                 <img src={AuthIcon} alt="auth-icon" title="auth-icon" />
                 로그아웃
                 {/* TODO: 추후에는 로그인 상태에 따라 동적으로 '로그인' 또는 '로그아웃' 텍스트가 보이도록 수정해야함. */}
