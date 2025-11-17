6f4977e239a4b868e453c193b37d48fee5f9ec8c
yuhyeon99 <jujini31@gmail.com>
2025-11-18 00:02:53 +0900

    feat(router): 회원 페이지 관련 라우팅 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
+import AuthPage from "./pages/AuthPage/index";
+import Login from "./pages/AuthPage/Login/index";
+import Register from "./pages/AuthPage/Register/index";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Header></Header>
+        <Routes>
+          <Route path="auth" element={<AuthPage />}>
+            <Route index element={<Login />} />
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+          </Route>
+        </Routes>
       </BrowserRouter>
     </>
   );
