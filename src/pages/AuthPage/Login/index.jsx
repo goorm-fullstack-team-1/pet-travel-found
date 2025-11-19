e75231e1ab161539067e0ad8b155cabc0cb6ba09
yuhyeon99 <jujini31@gmail.com>
2025-11-19 11:20:01 +0900

    feat: AuthForm 컴포넌트 적용

--- src/pages/AuthPage/Login/index.jsx
+++ src/pages/AuthPage/Login/index.jsx
@@ -1,36 +1,33 @@
 import styles from "./Login.module.css";
 import Button from "../../../components/Button/Button";
 import { Link } from "react-router-dom";
+import AuthFrom from "../components/AuthForm";
 
 const Login = () => {
-  return (
-    <>
-      <p className={styles.title}>로그인</p>
-      <p className={styles.description}>
-        Pet Travel Found에 오신 것을 환영합니다.
-      </p>
+  const textInfo = {
+    title: "로그인",
+    description: "Pet Travel Found에 오신 것을 환영합니다.",
+    lastMessage: "계정이 없으신가요?",
+  };
 
-      <form className={styles.auth_form}>
-        <label htmlFor="email"></label>
-        <input
-          type="email"
-          placeholder="이메일"
-          className={styles.auth_input}
-          id="email"
-        />
-        <label htmlFor="password"></label>
-        <input
-          type="password"
-          placeholder="비밀번호"
-          className={styles.auth_input}
-          id="password"
-        />
-        <Button>로그인</Button>
-      </form>
-      <p className={styles.footer_text}>
-        계정이 없으신가요? <Link to="/auth/register">회원가입</Link>
-      </p>
-    </>
+  return (
+    <AuthFrom textInfo={textInfo}>
+      <label htmlFor="email"></label>
+      <input
+        type="email"
+        placeholder="이메일"
+        className={styles.auth_input}
+        id="email"
+      />
+      <label htmlFor="password"></label>
+      <input
+        type="password"
+        placeholder="비밀번호"
+        className={styles.auth_input}
+        id="password"
+      />
+      <Button>로그인</Button>
+    </AuthFrom>
   );
 };
 
--- src/pages/AuthPage/Register/index.jsx
+++ src/pages/AuthPage/Register/index.jsx
@@ -1,39 +1,38 @@
 import Button from "../../../components/Button/Button";
 import styles from "./Register.module.css";
+import AuthFrom from "../components/AuthForm";
 
 const Register = () => {
+  const textInfo = {
+    title: "회원가입",
+    description: "계정을 생성하고 반려동물과 함께하는 여행을 시작하세요",
+    lastMessage: "이미 계정이 있으신가요?",
+  };
   return (
-    <>
-      <p className={styles.title}>회원가입</p>
-      <p className={styles.description}>
-        계정을 생성하고 반려동물과 함께하는 여행을 시작하세요.
-      </p>
-
-      <form className={styles.auth_form}>
-        <label htmlFor="name"></label>
-        <input
-          type="text"
-          placeholder="이름"
-          className={styles.auth_input}
-          id="name"
-        />
-        <label htmlFor="email"></label>
-        <input
-          type="email"
-          placeholder="이메일"
-          className={styles.auth_input}
-          id="email"
-        />
-        <label htmlFor="password"></label>
-        <input
-          type="password"
-          placeholder="비밀번호"
-          className={styles.auth_input}
-          id="password"
-        />
-        <Button>회원가입</Button>
-      </form>
-    </>
+    <AuthFrom textInfo={textInfo}>
+      <label htmlFor="name"></label>
+      <input
+        type="text"
+        placeholder="이름"
+        className={styles.auth_input}
+        id="name"
+      />
+      <label htmlFor="email"></label>
+      <input
+        type="email"
+        placeholder="이메일"
+        className={styles.auth_input}
+        id="email"
+      />
+      <label htmlFor="password"></label>
+      <input
+        type="password"
+        placeholder="비밀번호"
+        className={styles.auth_input}
+        id="password"
+      />
+      <Button>회원가입</Button>
+    </AuthFrom>
   );
 };
 
