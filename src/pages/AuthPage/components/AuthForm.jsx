0621d4aa3e162b51dca321c1ffa185babde0ae5b
yuhyeon99 <jujini31@gmail.com>
2025-11-19 11:24:13 +0900

    fix: 로그인/회원가입 페이지간 이동 코드 수정

--- src/pages/AuthPage/components/AuthForm.jsx
+++ src/pages/AuthPage/components/AuthForm.jsx
@@ -1,16 +1,21 @@
+import { Link } from "react-router-dom";
 import styles from "./AuthForm.module.css";
 
 const AuthFrom = ({ textInfo, children }) => {
   const { title, description, lastMessage } = textInfo;
 
   return (
-    <form>
+    <form className={styles.form}>
       <p>{title}</p>
       <p>{description}</p>
       <div className={styles.fields}>{children}</div>
       <p>
         {lastMessage}{" "}
-        <span>{title === "회원가입" ? "로그인" : "회원가입"}</span>
+        {title === "로그인" ? (
+          <Link to="/auth/register">회원가입</Link>
+        ) : (
+          <Link to="/auth/login">로그인</Link>
+        )}
       </p>
     </form>
   );
