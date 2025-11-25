1ab7e336548e6df77500efb9c8ae5eab21d05332
hee611 <th0611@kakao.com>
2025-11-25 20:22:00 +0900

    refactor: 로그아웃 기능 추가

--- src/pages/MyPage/components/UserInfo.jsx
+++ src/pages/MyPage/components/UserInfo.jsx
@@ -4,25 +4,36 @@ import Button from "../../../components/Button/Button";
 import {
   getUserName,
   getLoggedInUserEmail,
+  logout,
 } from "../../../services/auth/authService";
 
 const UserInfo = () => {
+  const userName = getUserName();
+  const userEmail = getLoggedInUserEmail();
+
+  const logoutHandler = () => {
+    logout();
+    window.location.href = "/";
+  };
+
   return (
     <div className={styles["user-info"]}>
-      {/* 추후 데이터 연동 필요 */}
       <div className={styles["user-info__header"]}>
         <img src={userIcon} alt="user icon" />
         <h1>회원 정보</h1>
       </div>
       <div className={styles["user-info__block"]}>
-        <h2>{getUserName()}</h2>
-        <p>{getLoggedInUserEmail()}</p>
+        <h2>{userName}</h2>
+        <p>{userEmail}</p>
       </div>
       <div className={styles["user-info__block"]}>
         <p>예약 내역</p>
+        {/* TODO: 추후 데이터 연동 필요 */}
         <h2>5</h2>
       </div>
-      <Button className={styles["user-info__button"]}>로그아웃</Button>
+      <Button className={styles["user-info__button"]} onClick={logoutHandler}>
+        로그아웃
+      </Button>
     </div>
   );
 };
