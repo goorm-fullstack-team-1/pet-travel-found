61fce797cdbf05c238b952f5d98dfc49bd151621
yuhyeon99 <jujini31@gmail.com>
2025-11-25 12:40:40 +0900

    refactor: 불필요 코드 제거(로그, 미사용 모듈 import)

--- src/pages/AuthPage/index.jsx
+++ src/pages/AuthPage/index.jsx
@@ -1,6 +1,6 @@
 import styles from "./AuthPage.module.css";
-import Button from "../../components/Button/Button";
 import { Outlet } from "react-router-dom";
+
 const AuthPage = () => {
   return (
     <div className={styles.auth_page}>
