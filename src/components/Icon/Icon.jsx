967a0a4f8e0dd88dc2e5046aee89a841fa9605e8
yuhyeon99 <jujini31@gmail.com>
2025-11-28 13:52:02 +0900

    refactor: 불필요 코드 제거

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -1,7 +1,6 @@
 import { iconMap } from "../../utils/iconLoader";
 
 const Icon = ({ name, className, ...props }) => {
-  console.log(iconMap);
   const SvgIcon = iconMap[name];
 
   if (!SvgIcon) {
