60813b85c0517c87e4bd8fdf7900bb70fa771c1b
yuhyeon99 <jujini31@gmail.com>
2025-11-28 13:15:48 +0900

    feat: Icon 컴포넌트 추가

---
+++ src/components/Icon/Icon.jsx
@@ -0,0 +1,15 @@
+import { iconMap } from "../../utils/iconLoader";
+
+const Icon = ({ name, className, ...props }) => {
+  console.log(iconMap);
+  const SvgIcon = iconMap[name];
+
+  if (!SvgIcon) {
+    console.warn(`❌ Icon "${name}" not found in icons folder.`);
+    return null;
+  }
+
+  return <SvgIcon className={className} {...props} />;
+};
+
+export default Icon;
