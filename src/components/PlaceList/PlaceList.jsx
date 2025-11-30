7d5f950f90c85919737eccdd1d0a8c3eb0e24e0f
hee611 <th0611@kakao.com>
2025-11-30 20:20:23 +0900

    refactor: 예외처리 및 가독성 향상을 위한 변수 추가

--- src/components/PlaceList/PlaceList.jsx
+++ src/components/PlaceList/PlaceList.jsx
@@ -9,10 +9,16 @@ const PlaceList = () => {
 
   useEffect(() => {
     const fetchPlaceData = async () => {
-      const request = await getPlaceList({ category: "펜션" });
-      const data = request?.response?.body?.items?.item;
+      try {
+        const request = await getPlaceList({ category: "펜션" });
+        const data = request?.response?.body?.items?.item;
 
-      setPlaceLists(data);
+        console.log(data);
+
+        setPlaceLists(data);
+      } catch (error) {
+        console.error("숙소 정보 불러오기를 실패했습니다.", error);
+      }
     };
 
     fetchPlaceData();
@@ -28,7 +34,7 @@ const PlaceList = () => {
           score={4.8}
           location={place.address}
           shortIntro={place.description}
-          options={[place.category1, place.category2]}
+          options={[place.category1, place.category2, place.category3]}
           price={"120,000"}
         ></PlaceItem>
       ))}
