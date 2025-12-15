19570cb9738758aff8005758d394f1a38c221247
yuhyeon99 <jujini31@gmail.com>
2025-12-15 14:08:04 +0900

    Revert "feat: 로그인된 사용자는 메인 페이지로 이동시키는 RedirectIfAUth 컴포넌트 추가"
    This reverts commit e2e39d4616cf9dcc9210ea257364dc1a81efcea8.

--- src/components/Guards/RedirectIfAuth.jsx
+++
@@ -1,8 +0,0 @@
-import { isAuthenticated } from "../../services/auth/authService";
-import { Navigate } from "react-router-dom";
-
-const RedirectIfAuth = ({ children }) => {
-  return isAuthenticated() ? <Navigate to="/" replace /> : children;
-};
-
-export default RedirectIfAuth;
